dd8009ad85f23ea3a2e1cd94bb4cee3c4c9b8302
Ken Amarapala <31244529+kamarapala@users.noreply.github.com>
2018-10-11 23:00:26 -0300

    Can now spawn testdummy.txt after highlighting tiles

--- _Project/ManipulateTileOnClick.cpp
+++ _Project/ManipulateTileOnClick.cpp
@@ -13,6 +13,8 @@
 #include "kitten\K_GameObject.h"
 
 #include "kitten\event_system\EventManager.h"
+#include "kibble\kibble.hpp"
+#include "unit\UnitSpawn.h"
 
 ManipulateTileOnClick::ManipulateTileOnClick()
 {
@@ -41,9 +43,11 @@ void ManipulateTileOnClick::onClick()
 	}
 	else if (ability::AbilityManager::getInstance()->lastAbilityUsed() == SUMMON_UNIT)
 	{
-		if (tileInfo->isHighlighted() && tileInfo->getOwnerId() == tileInfo->getHighlightedBy())
+		if (tileInfo->isHighlighted()) //&& tileInfo->getOwnerId() == tileInfo->getHighlightedBy())
 		{
-
+			kitten::K_GameObject* unit = unit::UnitSpawn::getInstance()->spawnUnitObject(kibble::getUnitDataParserInstance()->getUnit("testDummy.txt"));
+			unit->getTransform().place(tileInfo->getPosX() - 0.5f, -1.0f, tileInfo->getPosY());
+			kitten::EventManager::getInstance()->triggerEvent(kitten::Event::EventType::Unhighlight_Tile, nullptr);
 		}
 		else
 		{
