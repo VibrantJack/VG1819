645ef30367a35962b9a6c7ddc3f6b86462f93adc
Ken Amarapala <31244529+kamarapala@users.noreply.github.com>
2018-11-21 14:50:56 -0400

    New NetworkServices::createBuffer() to be tested

--- networking/NetworkServices.cpp
+++ networking/NetworkServices.cpp
@@ -11,11 +11,22 @@ namespace networking
 	int NetworkServices::sendMessage(SOCKET curSocket, char * message, int messageSize)
 	{
 		int iResult = send(curSocket, message, messageSize, 0);
+		//delete[] message; // If using createBuffer to create char * buffers
 		return iResult;
 	}
 
 	int NetworkServices::receiveMessage(SOCKET curSocket, char * buffer, int bufSize)
 	{
 		return recv(curSocket, buffer, bufSize, 0);
+	}
+
+
+	// Only use createBuffer if you will be sending the buffer through sendMessage which will delete char[]
+	Buffer& NetworkServices::createBuffer(int p_packetSize)
+	{
+		Buffer buffer;
+		buffer.m_data = new char[p_packetSize];
+		buffer.m_size = p_packetSize;
+		return buffer;
 	}
 }
