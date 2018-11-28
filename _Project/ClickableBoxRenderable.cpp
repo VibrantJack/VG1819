03dd5778e4433f81b5177dd4efd79edb0470eb8c
Callum <calmackenzie@upei.ca>
2018-11-27 20:43:22 -0400

    Debug renderable to see clickable boxes

---
+++ _Project/ClickableBoxRenderable.cpp
@@ -0,0 +1,114 @@
+#include "ClickableBoxRenderable.h"
+#include "kitten\K_GameObject.h"
+
+ClickableBoxRenderable::ClickableBoxRenderable() : m_vao(nullptr), m_mat(puppy::ShaderType::basic)
+{
+	m_mat.setTexture("textures/crap/cocacola.tga"); // Makes the lines red
+}
+
+ClickableBoxRenderable::~ClickableBoxRenderable()
+{
+	if (m_isEnabled)
+	{
+		onDisabled();
+	}
+
+	if (m_vao != nullptr)
+	{
+		delete m_vao;
+	}
+}
+
+void ClickableBoxRenderable::start()
+{
+	kitten::ClickableBox* clickableBox = m_attachedObject->getComponent<kitten::ClickableBox>();
+	assert(clickableBox != nullptr);
+
+	const glm::vec3& minPoint = clickableBox->getMinPoint();
+	const glm::vec3& maxPoint = clickableBox->getMaxPoint();
+	
+	puppy::TexturedVertex verts[] =
+	{
+		// Front
+		{ minPoint.x, minPoint.y, maxPoint.z,		0.0f, 0.0f },
+		{ minPoint.x,  maxPoint.y, maxPoint.z,		0.0f, 0.0f },
+		{ minPoint.x,  maxPoint.y, maxPoint.z,		0.0f, 0.0f },
+		{ maxPoint.x,  maxPoint.y, maxPoint.z,		0.0f, 0.0f },
+		{ maxPoint.x,  maxPoint.y, maxPoint.z,		0.0f, 0.0f },
+		{ maxPoint.x, minPoint.y, maxPoint.z,		0.0f, 0.0f },
+		{ maxPoint.x, minPoint.y, maxPoint.z,		0.0f, 0.0f },
+		{ minPoint.x, minPoint.y, maxPoint.z,		0.0f, 0.0f },
+
+		// Back
+		{ maxPoint.x,  maxPoint.y, minPoint.z,		0.0f, 0.0f },
+		{ minPoint.x,  maxPoint.y, minPoint.z,		0.0f, 0.0f },
+		{ minPoint.x,  maxPoint.y, minPoint.z,		0.0f, 0.0f },
+		{ minPoint.x, minPoint.y, minPoint.z,		0.0f, 0.0f },
+		{ minPoint.x, minPoint.y, minPoint.z,		0.0f, 0.0f },
+		{ maxPoint.x, minPoint.y, minPoint.z,		0.0f, 0.0f },
+		{ maxPoint.x, minPoint.y, minPoint.z,		0.0f, 0.0f },
+		{ maxPoint.x,  maxPoint.y, minPoint.z,		0.0f, 0.0f },
+
+		// Left
+		{ minPoint.x,  maxPoint.y, minPoint.z,		0.0f, 0.0f },
+		{ minPoint.x,  maxPoint.y, maxPoint.z,		0.0f, 0.0f },
+		{ minPoint.x,  maxPoint.y, maxPoint.z,		0.0f, 0.0f },
+		{ minPoint.x, minPoint.y, maxPoint.z,		0.0f, 0.0f },
+		{ minPoint.x, minPoint.y, maxPoint.z,		0.0f, 0.0f },
+		{ minPoint.x, minPoint.y, minPoint.z,		0.0f, 0.0f },
+		{ minPoint.x, minPoint.y, minPoint.z,		0.0f, 0.0f },
+		{ minPoint.x,  maxPoint.y, minPoint.z,		0.0f, 0.0f },
+
+		// Right
+		{ maxPoint.x,  maxPoint.y, maxPoint.z,		0.0f, 0.0f },
+		{ maxPoint.x,  maxPoint.y, minPoint.z,		0.0f, 0.0f },
+		{ maxPoint.x,  maxPoint.y, minPoint.z,		0.0f, 0.0f },
+		{ maxPoint.x, minPoint.y, minPoint.z,		0.0f, 0.0f },
+		{ maxPoint.x, minPoint.y, minPoint.z,		0.0f, 0.0f },
+		{ maxPoint.x, minPoint.y, maxPoint.z,		0.0f, 0.0f },
+		{ maxPoint.x, minPoint.y, maxPoint.z,		0.0f, 0.0f },
+		{ maxPoint.x,  maxPoint.y, maxPoint.z,		0.0f, 0.0f },
+
+		// Top
+		{ minPoint.x,  maxPoint.y, maxPoint.z,		0.0f, 0.0f },
+		{ minPoint.x,  maxPoint.y, minPoint.z,		0.0f, 0.0f },
+		{ minPoint.x,  maxPoint.y, minPoint.z,		0.0f, 0.0f },
+		{ maxPoint.x,  maxPoint.y, minPoint.z,		0.0f, 0.0f },
+		{ maxPoint.x,  maxPoint.y, minPoint.z,		0.0f, 0.0f },
+		{ maxPoint.x,  maxPoint.y, maxPoint.z,		0.0f, 0.0f },
+		{ maxPoint.x,  maxPoint.y, maxPoint.z,		0.0f, 0.0f },
+		{ minPoint.x,  maxPoint.y, maxPoint.z,		0.0f, 0.0f },
+
+		// Bottom
+		{ minPoint.x, minPoint.y, maxPoint.z,		0.0f, 0.0f },
+		{ maxPoint.x, minPoint.y, maxPoint.z,		0.0f, 0.0f },
+		{ maxPoint.x, minPoint.y, maxPoint.z,		0.0f, 0.0f },
+		{ maxPoint.x, minPoint.y, minPoint.z,		0.0f, 0.0f },
+		{ maxPoint.x, minPoint.y, minPoint.z,		0.0f, 0.0f },
+		{ minPoint.x, minPoint.y, minPoint.z,		0.0f, 0.0f },
+		{ minPoint.x, minPoint.y, minPoint.z,		0.0f, 0.0f },
+		{ minPoint.x, minPoint.y, maxPoint.z,		0.0f, 0.0f },
+	};
+
+	m_vao = new puppy::VertexEnvironment(verts, puppy::ShaderManager::getShaderProgram(puppy::ShaderType::basic), 48);
+
+	onEnabled();
+}
+
+void ClickableBoxRenderable::onEnabled()
+{
+	addToDynamicRender();
+}
+
+void ClickableBoxRenderable::onDisabled()
+{
+	removeFromDynamicRender();
+}
+
+void ClickableBoxRenderable::render(const glm::mat4& p_viewProj)
+{
+	m_mat.apply();
+	m_mat.setUniform(WORLD_VIEW_PROJ_UNIFORM_NAME, p_viewProj * getTransform().getWorldTransform());
+
+	m_vao->drawArrays(GL_LINES);
+}
---
+++ _Project/ClickableBoxRenderable.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "kitten\mouse picking\ClickableBox.h"
+#include "kitten\Renderable.h"
+#include "puppy\VertexEnvironment.h"
+#include "puppy\Material.h"
+
+class ClickableBoxRenderable : public kitten::Renderable
+{
+private:
+	virtual void start() override;
+	virtual void onEnabled() override;
+	virtual void onDisabled() override;
+
+	puppy::VertexEnvironment* m_vao;
+	puppy::Material m_mat;
+public:
+	ClickableBoxRenderable();
+	~ClickableBoxRenderable();
+
+	virtual void render(const glm::mat4& p_viewProj);
+};
