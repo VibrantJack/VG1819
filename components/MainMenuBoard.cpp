bc9f697b02b62acee917687c5dffca1740ce12b8
Ken Amarapala <31244529+kamarapala@users.noreply.github.com>
2018-12-13 11:50:41 -0400

    MainMenuBoard now spawns new simple unit game objects

--- components/MainMenuBoard.cpp
+++ components/MainMenuBoard.cpp
@@ -1,10 +1,21 @@
 #include "components\MainMenuBoard.h"
 
-#include "kitten\K_GameObject.h"
+#include "kitten\K_GameObjectManager.h"
 #include "unit\UnitSpawn.h"
 #include "unit\unitComponent\UnitMove.h"
 #include <time.h>
 #include <algorithm>
+
+#define SIMPLE_PRIEST "simple_units/Priest.txt"
+#define SIMPLE_ARCHER "simple_units/Archer.txt"
+#define SIMPLE_DUELIST "simple_units/Duelist.txt"
+#define SIMPLE_ENGINEER "simple_units/Engineer.txt"
+#define SIMPLE_NEWUNITOBJECT "simple_units/newUnitObject.txt"
+#define SIMPLE_PYROMANCER "simple_units/Pyromancer.txt"
+#define SIMPLE_STONESLINGER "simple_units/StoneSlinger.txt"
+#define SIMPLE_OLIGARCH "simple_units/Oligarch.txt"
+#define SIMPLE_KING "simple_units/King.txt"
+#define SIMPLE_QUEEN "simple_units/Queen.txt"
 
 MainMenuBoard::MainMenuBoard()
 {
@@ -17,24 +28,30 @@ MainMenuBoard::~MainMenuBoard()
 
 void MainMenuBoard::start()
 {
-	int units[] = { 
+	/*int units[] = { 
 		unit::UnitSpawn::Priest, unit::UnitSpawn::Archer, unit::UnitSpawn::Duelist, 
 		unit::UnitSpawn::Engineer, unit::UnitSpawn::newUnitObject, unit::UnitSpawn::Pyromancer, 
 		unit::UnitSpawn::StoneSlinger, unit::UnitSpawn::Oligarch, unit::UnitSpawn::King, unit::UnitSpawn::Queen 
 	};
-	std::random_shuffle(units, units + (sizeof(units)/sizeof(*units)));
+	std::random_shuffle(units, units + (sizeof(units)/sizeof(*units)));*/
 
-	kitten::K_GameObject* archer = unit::UnitSpawn::getInstanceSafe()->spawnSimpleUnitObject(units[0]); // Archer
-	archer->getComponent<unit::UnitMove>()->setTile(5, 4);
+	std::string simpleUnits[] = {
+		SIMPLE_PRIEST, SIMPLE_ARCHER, SIMPLE_DUELIST, SIMPLE_ENGINEER, SIMPLE_NEWUNITOBJECT,
+		SIMPLE_PYROMANCER, SIMPLE_STONESLINGER, SIMPLE_OLIGARCH, SIMPLE_KING, SIMPLE_QUEEN
+	};
+	std::random_shuffle(simpleUnits, simpleUnits + (sizeof(simpleUnits) / sizeof(*simpleUnits)));
 
-	kitten::K_GameObject* commander = unit::UnitSpawn::getInstanceSafe()->spawnSimpleUnitObject(units[1]); // Wizard/Commander
-	commander->getComponent<unit::UnitMove>()->setTile(6, 5);
+	kitten::K_GameObject* u0 = kitten::K_GameObjectManager::getInstance()->createNewGameObject(simpleUnits[0]);
+	u0->getTransform().move(5.0f, 0.0f, 4.5f);
 
-	kitten::K_GameObject* engineer = unit::UnitSpawn::getInstanceSafe()->spawnSimpleUnitObject(units[2]); // Engineer
-	engineer->getComponent<unit::UnitMove>()->setTile(8, 5);
+	kitten::K_GameObject* u1 = kitten::K_GameObjectManager::getInstance()->createNewGameObject(simpleUnits[1]);
+	u1->getTransform().move(6.0f, 0.0f, 5.5f);
 
-	kitten::K_GameObject* duelist = unit::UnitSpawn::getInstanceSafe()->spawnSimpleUnitObject(units[3]); // Duelist
-	duelist->getComponent<unit::UnitMove>()->setTile(9, 4);	
+	kitten::K_GameObject* u2 = kitten::K_GameObjectManager::getInstance()->createNewGameObject(simpleUnits[2]);
+	u2->getTransform().move(8.0f, 0.0f, 5.5f);
 
-	//printf("Spawned units: %d, %d, %d, %d\n", units[0], units[1], units[2], units[3]);
+	kitten::K_GameObject* u3 = kitten::K_GameObjectManager::getInstance()->createNewGameObject(simpleUnits[3]);
+	u3->getTransform().move(9.0f, 0.0f, 4.5f);
+
+	//printf("Spawned units: %d, %d, %d, %d\n", simpleUnits[0], simpleUnits[1], simpleUnits[2], simpleUnits[3]);
 }
