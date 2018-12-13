018037986c912372d243b261a30874814174fe8c
Ken Amarapala <31244529+kamarapala@users.noreply.github.com>
2018-12-13 10:41:15 -0400

    Randomly generated units for main menu

--- components/MainMenuBoard.cpp
+++ components/MainMenuBoard.cpp
@@ -3,11 +3,12 @@
 #include "kitten\K_GameObject.h"
 #include "unit\UnitSpawn.h"
 #include "unit\unitComponent\UnitMove.h"
-
+#include <time.h>
+#include <algorithm>
 
 MainMenuBoard::MainMenuBoard()
 {
-
+	srand(time(NULL));
 }
 
 MainMenuBoard::~MainMenuBoard()
@@ -16,15 +17,24 @@ MainMenuBoard::~MainMenuBoard()
 
 void MainMenuBoard::start()
 {
-	kitten::K_GameObject* archer = unit::UnitSpawn::getInstanceSafe()->spawnSimpleUnitObject(2); // Archer
+	int units[] = { 
+		unit::UnitSpawn::Priest, unit::UnitSpawn::Archer, unit::UnitSpawn::Duelist, 
+		unit::UnitSpawn::Engineer, unit::UnitSpawn::newUnitObject, unit::UnitSpawn::Pyromancer, 
+		unit::UnitSpawn::StoneSlinger, unit::UnitSpawn::Oligarch, unit::UnitSpawn::King, unit::UnitSpawn::Queen 
+	};
+	std::random_shuffle(units, units + (sizeof(units)/sizeof(*units)));
+
+	kitten::K_GameObject* archer = unit::UnitSpawn::getInstanceSafe()->spawnSimpleUnitObject(units[0]); // Archer
 	archer->getComponent<unit::UnitMove>()->setTile(5, 4);
 
-	kitten::K_GameObject* commander = unit::UnitSpawn::getInstanceSafe()->spawnSimpleUnitObject(6); // Wizard/Commander
+	kitten::K_GameObject* commander = unit::UnitSpawn::getInstanceSafe()->spawnSimpleUnitObject(units[1]); // Wizard/Commander
 	commander->getComponent<unit::UnitMove>()->setTile(6, 5);
 
-	kitten::K_GameObject* engineer = unit::UnitSpawn::getInstanceSafe()->spawnSimpleUnitObject(4); // Engineer
+	kitten::K_GameObject* engineer = unit::UnitSpawn::getInstanceSafe()->spawnSimpleUnitObject(units[2]); // Engineer
 	engineer->getComponent<unit::UnitMove>()->setTile(8, 5);
 
-	kitten::K_GameObject* duelist = unit::UnitSpawn::getInstanceSafe()->spawnSimpleUnitObject(3); // Duelist
+	kitten::K_GameObject* duelist = unit::UnitSpawn::getInstanceSafe()->spawnSimpleUnitObject(units[3]); // Duelist
 	duelist->getComponent<unit::UnitMove>()->setTile(9, 4);	
+
+	//printf("Spawned units: %d, %d, %d, %d\n", units[0], units[1], units[2], units[3]);
 }
