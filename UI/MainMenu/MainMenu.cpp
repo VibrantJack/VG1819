883c1cecdc461ad600933e4de032c57ce7cb2467
Ken Amarapala <31244529+kamarapala@users.noreply.github.com>
2018-12-22 12:31:51 -0400

    Created json files for main menu buttons

--- UI/MainMenu/MainMenu.cpp
+++ UI/MainMenu/MainMenu.cpp
@@ -1,3 +1,5 @@
+// Testing Main Menu animations
+
 #include "UI\MainMenu\MainMenu.h"
 #include "kitten\K_GameObjectManager.h"
 
@@ -17,27 +19,35 @@ void MainMenu::start()
 	assert(m_input != nullptr);
 
 	m_menuTop = kitten::K_GameObjectManager::getInstance()->createNewGameObject("UI/menu_top.json");
+	m_menuTop->getTransform().move(0.0f, 0.0f, -0.9f);
 	m_menuTopController = m_menuTop->getComponent<LerpController>();
 	m_menuTopController->addPositionLerpFinishedCallback(this);
 
 	m_menuBottom = kitten::K_GameObjectManager::getInstance()->createNewGameObject("UI/menu_bottom.json");
+	m_menuBottom->getTransform().move(0.0f, 0.0f, -0.9f);
 	m_menuBottomController = m_menuBottom->getComponent<LerpController>();
-	//m_menuBottomController->addPositionLerpFinishedCallback(this);
+
+	m_title = kitten::K_GameObjectManager::getInstance()->createNewGameObject("UI/victorum_title.json");
+	m_title->getTransform().move(0.0f, 0.0f, -0.1f);
+	m_titleController = m_title->getComponent<LerpController>();
+	
 }
 
 void MainMenu::update()
 {
 	if (m_input->keyDown(GLFW_KEY_TAB) && !m_input->keyDownLast(GLFW_KEY_TAB))
 	{
+		m_titleController->positionLerp(glm::vec3(306.0f, 720.0f, -0.1f), MENU_LERP_TIME);
 		m_menuTopController->positionLerp(MENU_TOP_LERP_VEC3_POS, MENU_LERP_TIME);
 		m_menuBottomController->positionLerp(MENU_BOTTOM_LERP_VEC3_POS, MENU_LERP_TIME);
+
 		m_menuTopController->addPositionLerpFinishedCallback(this);
 	}
 }
 
 void MainMenu::onPositionLerpFinished()
 {
+	m_titleController->positionLerp(TITLE_ORIGINAL_VEC3_POS, MENU_LERP_TIME);
 	m_menuTopController->positionLerp(MENU_TOP_ORIGINAL_VEC3_POS, MENU_LERP_TIME);
 	m_menuBottomController->positionLerp(MENU_BOTTOM_ORIGINAL_VEC3_POS, MENU_LERP_TIME);
-
 }
--- UI/MainMenu/MainMenu.h
+++ UI/MainMenu/MainMenu.h
@@ -4,11 +4,13 @@
 #include "UI\UIObject.h"
 #include "_Project\LerpController.h"
 
-#define MENU_TOP_ORIGINAL_VEC3_POS glm::vec3(0.0f, 560.0f, 0.0f)
-#define MENU_BOTTOM_ORIGINAL_VEC3_POS glm::vec3(0.0f, -225.0f, 0.0f)
+#define TITLE_ORIGINAL_VEC3_POS glm::vec3(306.0f, 528.0f, -0.1f)
+#define MENU_TOP_ORIGINAL_VEC3_POS glm::vec3(0.0f, 560.0f, -0.9f)
+#define MENU_BOTTOM_ORIGINAL_VEC3_POS glm::vec3(0.0f, -225.0f, -0.9f)
 
-#define MENU_TOP_LERP_VEC3_POS glm::vec3(0.0f, 360.0f, 0.0f)
-#define MENU_BOTTOM_LERP_VEC3_POS glm::vec3(0.0f, 0.0f, 0.0f)
+#define TITLE_LERP_VEC3_POS glm::vec3(306.0f, 328.0f, -0.1f)
+#define MENU_TOP_LERP_VEC3_POS glm::vec3(0.0f, 360.0f, -0.9f)
+#define MENU_BOTTOM_LERP_VEC3_POS glm::vec3(0.0f, 0.0f, -0.9f)
 
 #define MENU_LERP_TIME 0.75f
 
@@ -18,9 +20,11 @@ private:
 	input::InputManager* m_input;
 	LerpController* m_menuTopController;
 	LerpController* m_menuBottomController;
+	LerpController* m_titleController;
 
 	kitten::K_GameObject* m_menuTop;
 	kitten::K_GameObject* m_menuBottom;
+	kitten::K_GameObject* m_title;
 
 public:
 	MainMenu();
