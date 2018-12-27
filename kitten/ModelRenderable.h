a20c81360f5f87643fea8e4428645ccf902e25bd
Callum <calmackenzie@upei.ca>
2018-12-27 15:37:19 -0400

    placed a bunch of trees and cached models to speed up loading and save memory

--- kitten/ModelRenderable.h
+++ kitten/ModelRenderable.h
@@ -3,10 +3,13 @@
 #include "kitten\K_Renderable.h"
 #include "puppy\Models\P_Model.h"
 
+#include <string>
+
 class ModelRenderable : public kitten::K_Renderable
 {
 private:
-	puppy::P_Model m_model;
+	static std::unordered_map<std::string, puppy::P_Model*> m_loadedModels;
+	puppy::P_Model* m_model;
 
 	virtual void start() override;
 	virtual void onEnabled() override;
