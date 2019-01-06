c87d7b3651199c08d4fe5f539639dce76a2f91df
SRJYC <you0rock0@gmail.com>
2019-01-06 16:05:08 -0400

    change display current unit

--- unit/InitiativeTracker/UnitAura.cpp
+++ unit/InitiativeTracker/UnitAura.cpp
@@ -2,8 +2,11 @@
 #include "kitten/K_GameObjectManager.h"
 #include "kibble/kibble.hpp"
 
-unit::UnitAura::UnitAura()
+unit::UnitAura::UnitAura(float p_speed, float p_max, float p_min)
+	:m_speed(p_speed),m_max(p_max),m_min(p_min)
 {
+	m_scale = 1.0;
+	m_pos = true;
 }
 
 unit::UnitAura::~UnitAura()
@@ -21,5 +24,18 @@ bool unit::UnitAura::hasUpdate() const
 
 void unit::UnitAura::update()
 {
-	m_attachedObject->getTransform().rotateRelative(glm::vec3(0, m_speed, 0));
+	if (m_pos)
+	{
+		m_scale += m_speed;
+		if (m_scale >= m_max)
+			m_pos = false;
+	}
+	else
+	{
+		m_scale -= m_speed;
+		if (m_scale <= m_min)
+			m_pos = true;
+	}
+
+	m_attachedObject->getTransform().scaleAbsolute(m_scale,1,m_scale);
 }
