49eb17d652ff96db91d6ece3b9b874cc9129e73f
Saleh Alhakeem <vibrantjack@outlook.com>
2019-02-09 19:25:08 -0400

    Deck now deletes properly.

--- components/clickables/DeleteDeckOnClick.cpp
+++ components/clickables/DeleteDeckOnClick.cpp
@@ -2,6 +2,7 @@
 #include "components/DeckAlterationComponent.h"
 #include "components/DecksDisplayFrame.h"
 #include "kibble/databank/databank.hpp"
+#include "_Project/DisableAfterTime.h"
 
 DeleteDeckOnClick::DeleteDeckOnClick() { }
 
@@ -9,9 +10,27 @@ DeleteDeckOnClick::~DeleteDeckOnClick() {
 
 }
 
-void DeleteDeckOnClick::onClick()
+void DeleteDeckOnClick::onSuccess()
 {
 	int deckId = DecksDisplayFrame::getActiveInstance()->getCurrentPickedDeckId();
 	kibble::eraseDeckData(deckId);
 	DecksDisplayFrame::getActiveInstance()->updateDisplay();
+	PromptPopUp::getActiveInstance()->setEnabledPrompt(false);
+
+	if (m_message == nullptr)
+	{
+		m_message = kitten::K_GameObjectManager::getInstance()->createNewGameObject("deleted_deck_message.json");
+	}
+	else
+	{
+		m_message->setEnabled(true);
+	}
+	m_message->getComponent<DisableAfterTime>()->setTime(3.0);
+}
+
+void DeleteDeckOnClick::onClick()
+{
+	m_text = "Are you sure you want to delete this deck there, buddy?";
+	PromptPopUp::getActiveInstance()->setEnabledPrompt(true);
+	PromptPopUp::getActiveInstance()->setBehavior(this);
 }
