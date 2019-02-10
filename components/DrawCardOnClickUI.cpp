bdd5545678381183251a272bdfb46a60a862b168
Saleh Alhakeem <vibrantjack@outlook.com>
2019-02-09 23:21:56 -0400

    Added Check

--- components/DrawCardOnClickUI.cpp
+++ components/DrawCardOnClickUI.cpp
@@ -1,10 +1,13 @@
 #include "DrawCardOnClickUI.h"
 #include "kitten/event_system/EventManager.h"
+#include "UI/HandFrame.h"
 
 #include <iostream>
 void DrawCardOnClickUI::onClick() {
-	kitten::Event *e = new kitten::Event(kitten::Event::EventType::Draw_Card);// make a draw event
-	e->putInt(PLAYER_ID, 0); // give it a player id, 0 being default TODO change this if ever needed. 0 signifies this side's instance player's id. any other should be any number other than 0 
-	e->putInt(CARD_COUNT, 1); //tell it to draw 1 card
-	kitten::EventManager::getInstance()->triggerEvent(kitten::Event::EventType::Draw_Card, e); // Call the event
+	if (userinterface::HandFrame::getActiveInstance()->getTotalCardsInHand() < 5) {
+		kitten::Event *e = new kitten::Event(kitten::Event::EventType::Draw_Card);// make a draw event
+		e->putInt(PLAYER_ID, 0); // give it a player id, 0 being default TODO change this if ever needed. 0 signifies this side's instance player's id. any other should be any number other than 0 
+		e->putInt(CARD_COUNT, 1); //tell it to draw 1 card
+		kitten::EventManager::getInstance()->triggerEvent(kitten::Event::EventType::Draw_Card, e); // Call the event
+	}
 }
--- unit/unitComponent/Commander.cpp
+++ unit/unitComponent/Commander.cpp
@@ -1,6 +1,6 @@
 #include "Commander.h"
 #include "unitInteraction/UnitInteractionManager.h"
-
+#include "UI/HandFrame.h"
 
 // Networking
 #include "networking\ClientGame.h"
@@ -93,10 +93,12 @@ namespace unit
 				kitten::EventManager::getInstance()->triggerEvent(kitten::Event::Reset_Power, nullptr);
 
 				// Draw a card
-				kitten::Event *e = new kitten::Event(kitten::Event::EventType::Draw_Card);// make a draw event
-				e->putInt(PLAYER_ID, 0); // give it a player id, 0 being default TODO change this if ever needed. 0 signifies this side's instance player's id. any other should be any number other than 0 
-				e->putInt(CARD_COUNT, 2); //tell it to draw 1 card
-				kitten::EventManager::getInstance()->triggerEvent(kitten::Event::EventType::Draw_Card, e); // Call the event
+				if (userinterface::HandFrame::getActiveInstance()->getTotalCardsInHand() < 5) {
+					kitten::Event *e = new kitten::Event(kitten::Event::EventType::Draw_Card);// make a draw event
+					e->putInt(PLAYER_ID, 0); // give it a player id, 0 being default TODO change this if ever needed. 0 signifies this side's instance player's id. any other should be any number other than 0 
+					e->putInt(CARD_COUNT, 2); //tell it to draw 1 card
+					kitten::EventManager::getInstance()->triggerEvent(kitten::Event::EventType::Draw_Card, e); // Call the event
+				}
 			}
 		}
 		else
