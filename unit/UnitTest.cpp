75cc69bfa606162d3b534b913cf7db4f63c44dab
SRJYC <you0rock0@gmail.com>
2019-02-11 06:38:34 -0400

    change engineer and priest sprites sheet

--- unit/UnitTest.cpp
+++ unit/UnitTest.cpp
@@ -90,20 +90,20 @@ namespace unit
 		
 		//set initial position
 
-		kitten::K_GameObject* u0 = UnitSpawn::getInstance()->spawnUnitObject(2);//archer
-		u0->getComponent<unit::UnitMove>()->setTile(3, 3);
+//		kitten::K_GameObject* u0 = UnitSpawn::getInstance()->spawnUnitObject(2);//archer
+//		u0->getComponent<unit::UnitMove>()->setTile(3, 3);
 
-//		kitten::K_GameObject* u1 = UnitSpawn::getInstance()->spawnUnitObject(1);//priest
-//		u1->getComponent<unit::UnitMove>()->setTile(2, 2);
+		kitten::K_GameObject* u1 = UnitSpawn::getInstance()->spawnUnitObject(1);//priest
+		u1->getComponent<unit::UnitMove>()->setTile(2, 2);
 
-		kitten::K_GameObject* u2 = UnitSpawn::getInstance()->spawnUnitObject(2);//archer
-		u2->getComponent<unit::UnitMove>()->setTile(3, 2);
+//		kitten::K_GameObject* u2 = UnitSpawn::getInstance()->spawnUnitObject(2);//archer
+//		u2->getComponent<unit::UnitMove>()->setTile(3, 2);
 
 //		kitten::K_GameObject* u3 = UnitSpawn::getInstance()->spawnUnitObject(3);//duelist
 //		u3->getComponent<unit::UnitMove>()->setTile(4, 2);
 
-//		kitten::K_GameObject* u4 = UnitSpawn::getInstance()->spawnUnitObject(4);//engineer
-//		u4->getComponent<unit::UnitMove>()->setTile(5, 2);
+		kitten::K_GameObject* u4 = UnitSpawn::getInstance()->spawnUnitObject(4);//engineer
+		u4->getComponent<unit::UnitMove>()->setTile(5, 2);
 		
 //		kitten::K_GameObject* u5 = UnitSpawn::getInstance()->spawnUnitObject(7);//pyromancer
 //		u5->getComponent<unit::UnitMove>()->setTile(6, 2);
@@ -120,10 +120,10 @@ namespace unit
 //		kitten::K_GameObject* u9 = UnitSpawn::getInstance()->spawnUnitObject(11);//divine status
 //		u9->getComponent<unit::UnitMove>()->setTile(10, 2);
 		
-		kitten::K_GameObject* u10 = UnitSpawn::getInstance()->spawnUnitObject(12);//enchanter
-		u10->getComponent<unit::UnitMove>()->setTile(10, 3);
-		u10->getComponent<unit::Unit>()->m_attributes[UNIT_LV] = 3;
-		u10->getComponent<unit::Unit>()->m_attributes[COUNTER_ENERGY] = 4;
+//		kitten::K_GameObject* u10 = UnitSpawn::getInstance()->spawnUnitObject(12);//enchanter
+//		u10->getComponent<unit::UnitMove>()->setTile(10, 3);
+//		u10->getComponent<unit::Unit>()->m_attributes[UNIT_LV] = 3;
+//		u10->getComponent<unit::Unit>()->m_attributes[COUNTER_ENERGY] = 4;
 
 //		kitten::K_GameObject* u11 = UnitSpawn::getInstance()->spawnUnitObject(5);//wall
 //		u11->getComponent<unit::UnitMove>()->setTile(10, 8);
