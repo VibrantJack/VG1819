5bed51a6176e206c9dfb629fa7d62b3f973d285d
Callum <calmackenzie@upei.ca>
2019-03-11 23:02:25 -0300

    arm fix

--- ability/ability/Arm.cpp
+++ ability/ability/Arm.cpp
@@ -20,14 +20,13 @@ namespace ability
 				{
 					//apply Status_Encourage to target
 					ability::Status* se = ability::StatusManager::getInstance()->findStatus(STATUS_SHIELD);
-					addStatusInfo(se, p_info);					
+								
 					UniversalPfx::getInstance()->playEffect("Arm", it->getTransform().getTranslation());
 
 					std::vector<std::string> intKeys;
 					intKeys.push_back(UNIT_POWER);
 
-					se->m_source = ABILITY_ARM;
-					se->addCounter(UNIT_POWER, pow);
+					addStatusInfo(se, p_info, intKeys);
 
 					//attach to target
 					se->attach(it);
