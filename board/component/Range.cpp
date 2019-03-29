ae68e5b0b28c5c8c85742b52cf0aa51d4efacb64
SRJYC <you0rock0@gmail.com>
2019-03-29 12:14:29 -0300

    range component update

--- board/component/Range.cpp
+++ board/component/Range.cpp
@@ -1,7 +1,7 @@
 #include "Range.h"
 #include "board/tile/TileInfo.h"
 #include "board/BoardManager.h"
-
+#include <iostream>
 
 Range::Range()
 {
@@ -20,135 +20,137 @@ void Range::setDimension(int p_x, int p_z)
 kitten::Event::TileList Range::getTilesInRange(kitten::Event * p_data)
 {
 	kitten::K_GameObject * tileAtOrigin = p_data->getGameObj("tileAtOrigin");
-	int min = p_data->getInt("min_range");
-	int max = p_data->getInt("max_range");
+	m_min = p_data->getInt("min_range");
+	m_max = p_data->getInt("max_range");
+	m_origin = tileAtOrigin->getComponent<TileInfo>()->getPos();
 
-	//get list in range
-	kitten::Event::TileList list = getTilesInRange(tileAtOrigin, min, max);
-
-	/*using filter instead
-	//remove tiles that has something on it if this is used for moving
-	std::string use = p_data->getString("use");
-	if (use == "move")
+	if (p_data->getInt("path") == TRUE)
 	{
-		removeUnit(&list);
+		return getPathRange();
 	}
-	else if (use == "ManipulateTile")
-	{
-		removeOwned(&list);
-	}*/
 
-	return list;
+	return getList();
 }
 
 kitten::Event::TileList Range::getTilesInRange(kitten::K_GameObject * p_tileAtOrigin, int p_minRange, int p_maxRange)
 {
-	std::map<std::pair<int, int>, int> tilesAndRange;
+	m_min = p_minRange;
+	m_max = p_maxRange;
+	m_origin = p_tileAtOrigin->getComponent<TileInfo>()->getPos();
 
-	//find the coordinates for the tile at origin
-	std::pair<int, int> originCoord = p_tileAtOrigin->getComponent<TileInfo>()->getPos();
+	return getList();
+}
 
-	//find tiles
-	findNeighbour(&tilesAndRange,originCoord,0,p_minRange,p_maxRange);
-
-	//add it to the highlight list 
+kitten::Event::TileList Range::getList() const
+{
 	kitten::Event::TileList list;
-	for (auto it = tilesAndRange.begin(); it!=tilesAndRange.end();it++)
+
+	for (int xStep = m_max; xStep >= -m_max; xStep--)
 	{
-		int d = it->second;
-		//if the distance is within the range
-		if (d >= p_minRange && d <= p_maxRange)
+		int alter = m_max - xStep;
+		if (xStep < 0)
+			alter = xStep + m_max;
+
+		for (int zStep = -alter; zStep <= alter; zStep++)
 		{
-			list.push_back(it->first);
+			int x = m_origin.first + xStep;
+			int z = m_origin.second + zStep;
+			std::cout << "tile:" << x << " , " << z << std::endl;
+			addTile(&list, x, z);
 		}
 	}
 
 	return list;
 }
 
-void Range::findNeighbour(std::map<std::pair<int, int>, int>* p_tilesAndRange, std::pair<int, int> p_currentTile, int p_distance, int p_minRange, int p_maxRange)
+void Range::addTile(kitten::Event::TileList * p_list, int p_tileX, int p_tileZ) const
 {
+	//within dimension
+	if (p_tileX >= 0 && p_tileX < m_x && p_tileZ >= 0 && p_tileZ < m_z)
+	{
+		//within range
+		int distance = 0;
+		distance += std::abs(p_tileX - m_origin.first);
+		distance += std::abs(p_tileZ - m_origin.second);
+		if (distance >= m_min && distance <= m_max)
+		{
+			p_list->push_back(std::make_pair(p_tileX,p_tileZ));
+		}
+	}
+}
+
+kitten::Event::TileList Range::getPathRange() const
+{
+	std::map<std::pair<int, int>, int> tilesAndRange;
+
+	int x = m_origin.first;
+	int z = m_origin.second;
+
+	checkTile(&tilesAndRange, std::pair<int, int>(x - 1, z), 0);
+	checkTile(&tilesAndRange, std::pair<int, int>(x + 1, z), 0);
+	checkTile(&tilesAndRange, std::pair<int, int>(x, z - 1), 0);
+	checkTile(&tilesAndRange, std::pair<int, int>(x, z + 1), 0);
+
+	kitten::Event::TileList list;
+	for (auto it : tilesAndRange)
+	{
+		if (it.second >= m_min && it.second <= m_max)
+		{
+			list.push_back(it.first);
+		}
+	}
+	return list;
+}
+
+void Range::checkTile(std::map<std::pair<int, int>, int>* p_tilesAndRange, std::pair<int, int> p_currentTile, int p_distance) const
+{
+	//out of board
+	if (p_currentTile.first >= m_x || p_currentTile.first < 0 || p_currentTile.second >= m_z || p_currentTile.second < 0)
+		return;
+
+	//get tile info
+	TileInfo* info = BoardManager::getInstance()->getTile(p_currentTile.first, p_currentTile.second)->getComponent<TileInfo>();
+
+	//dont add tile has unit exclude origin tile
+	if (info->hasUnit())
+		return;
+
+	//get mv cost
+	int cost = info->getMVCost();
+
+	//distance is preivouse tile cost,add it
+	cost += p_distance;
+
 	//if the distance is excess the max range, we dont go further
-	if (p_distance > p_maxRange)
+	if (cost > m_max)
 		return;
 
 	//check if the tile is already in the list
-	bool exist = p_tilesAndRange->find(p_currentTile) != p_tilesAndRange->end();
-	if (exist)
+	auto found = p_tilesAndRange->find(p_currentTile);
+	if (found != p_tilesAndRange->end())
 	{
 		//it's in the list, then compare the distance
-		if (p_distance < (*p_tilesAndRange)[p_currentTile])
+		if (cost < found->second)
 		{
-			(*p_tilesAndRange)[p_currentTile] = p_distance;
+			found->second = cost;
 			//always keep the least distance
 		}
-		else
+		else//avoid repeat check
 			return;
-		//if the list already has smaller distance, terminal
-
 	}
 	else
 	{
-		//not find,then add it to the list
-		p_tilesAndRange->insert(std::pair<std::pair<int, int>, int>(p_currentTile, p_distance));
+		//not in the list, add it
+		p_tilesAndRange->insert(std::make_pair(p_currentTile, cost));
 	}
 
 	//then check neighbour
 	int x = p_currentTile.first;
 	int z = p_currentTile.second;
 
-	if (x > 0)//check right
-	{
-		findNeighbour(p_tilesAndRange, std::pair<int, int>(x-1, z), p_distance + 1, p_minRange, p_maxRange);
-	}
-	if (x < (m_x - 1))//check left
-	{
-		findNeighbour(p_tilesAndRange, std::pair<int, int>(x+1, z), p_distance + 1, p_minRange, p_maxRange);
-	}
-	if (z > 0)//check down
-	{
-		findNeighbour(p_tilesAndRange, std::pair<int, int>(x, z-1), p_distance + 1, p_minRange, p_maxRange);
-	}
-	if (z < (m_z - 1))//check up
-	{
-		findNeighbour(p_tilesAndRange, std::pair<int, int>(x, z+1), p_distance + 1, p_minRange, p_maxRange);
-	}
+	checkTile(p_tilesAndRange, std::pair<int, int>(x - 1, z), cost);
+	checkTile(p_tilesAndRange, std::pair<int, int>(x + 1, z), cost);
+	checkTile(p_tilesAndRange, std::pair<int, int>(x, z - 1), cost);
+	checkTile(p_tilesAndRange, std::pair<int, int>(x, z + 1), cost);
 }
-/*
-void Range::removeUnit(kitten::Event::TileList* p_list)
-{
-	//remove tiles that has unit
-	auto it = p_list->begin();
-	while (it != p_list->end())
-	{
-		kitten::K_GameObject* tileGO = BoardManager::getInstance()->getTile(it->first, it->second);
-		if (tileGO->getComponent<TileInfo>()->hasUnit())
-		{
-			it = p_list->erase(it);
-		}
-		else
-		{
-			it++;
-		}
-	}
-}
-
-void Range::removeOwned(kitten::Event::TileList * p_list)
-{
-	//remove tiles that has owner
-	auto it = p_list->begin();
-	while (it != p_list->end())
-	{
-		kitten::K_GameObject* tileGO = BoardManager::getInstance()->getTile(it->first, it->second);
-		std::string owner = tileGO->getComponent<TileInfo>()->getOwnerId();
-		if (owner != DEFAULT_OWNER)
-		{
-			it = p_list->erase(it);
-		}
-		else
-		{
-			it++;
-		}
-	}
-}*/
 
--- board/component/Range.h
+++ board/component/Range.h
@@ -21,18 +21,22 @@ public:
 
 	kitten::Event::TileList getTilesInRange(kitten::Event * p_data);
 
-	kitten::Event::TileList getTilesInRange
-	(kitten::K_GameObject* p_tileAtOrigin,
-		int p_minRange, int p_maxRange);
+	kitten::Event::TileList getTilesInRange(kitten::K_GameObject* p_tileAtOrigin,int p_minRange, int p_maxRange);
 private:
 	int m_x;
 	int m_z;
 
-	void findNeighbour
-	(std::map<std::pair<int, int>, int>* p_tilesAndRange, 
-		std::pair<int, int> p_currentTile, 
-		int p_distance, int p_minRange, int p_maxRange);
+	int m_min;
+	int m_max;
 
-	//void removeUnit(kitten::Event::TileList* p_list);
-	//void removeOwned(kitten::Event::TileList* p_list);
+	std::pair<int, int> m_origin;
+
+	kitten::Event::TileList getList() const;
+	void addTile(kitten::Event::TileList* p_list, int p_tileX, int p_tileZ) const;
+
+	kitten::Event::TileList getPathRange() const;
+	void checkTile
+	(std::map<std::pair<int, int>, int>* p_tilesAndRange,
+		std::pair<int, int> p_currentTile,
+		int p_distance) const;
 };
